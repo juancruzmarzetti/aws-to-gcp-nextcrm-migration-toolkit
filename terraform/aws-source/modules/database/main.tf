48f54f8a71c4c46cc67d650d1bfcb91b743ae55f
juancruzmarzetti <naujmarz@gmail.com>
2026-05-21 12:17:25 -0300

    postgres-db-version-fixed-for-pgvector-compatibility

--- terraform/aws-source/modules/database/main.tf
+++ terraform/aws-source/modules/database/main.tf
@@ -3,12 +3,12 @@ resource "aws_db_instance" "nextcrm_db" {
   allocated_storage          = 20
   max_allocated_storage      = 50 # allow auto-scaling up to 50 GB
   engine                     = "postgres"
-  engine_version             = "16"
+  engine_version             = "17"
   instance_class             = var.db_instance_type
   db_name                    = var.db_name
   username                   = var.db_username
   password                   = var.db_password
-  parameter_group_name       = "default.postgres16"
+  parameter_group_name       = "default.postgres17"
   vpc_security_group_ids     = var.security_group_ids
   db_subnet_group_name       = aws_db_subnet_group.nextcrm_db_subnet_group.name
   skip_final_snapshot        = true
